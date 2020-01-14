065767c8a84ecf7723cda36b03b4944c2129ea42
William (NB) <william.wen@gmail.com>
2020-01-14 09:00:59 +0800

    Add project files.

---
+++ w9wen.OPC.UA.Client.ConsoleApp/EntryClient.cs
@@ -0,0 +1,32 @@
+﻿using Opc.Ua;
+using Opc.Ua.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w9wen.OPC.UA.Client.ConsoleApp
+{
+    public class EntryClient
+    {
+        public void Run()
+        {
+            Instance().Wait();
+        }
+
+        private async Task Instance()
+        {
+            Console.WriteLine("aaa");
+
+            ApplicationInstance application = new ApplicationInstance
+            {
+                ApplicationName = "OPC UA Client",
+                ApplicationType = ApplicationType.Client,
+                ConfigSectionName = Utils.IsRunningOnMono() ? "Opc.Ua.MonoSampleClient" : "Opc.Ua.SampleClient"
+            };
+
+            // load the application configuration.
+            ApplicationConfiguration config = await application.LoadApplicationConfiguration(false);
+        }
+    }
+}
---
+++ w9wen.OPC.UA.Client.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace w9wen.OPC.UA.Client.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var entryClient = new EntryClient();
+            entryClient.Run();
+        }
+    }
+}
